ddd63b6ef5eac42a9927ba2386a3779c7d6f5303
lan.ta <lan.ta@c-mg.com>
2015-06-10 09:43:50 +0100

    update

--- public/js/pension_accumulators/cmg_formula.js
+++ public/js/pension_accumulators/cmg_formula.js
@@ -1,5 +1,5 @@
 function parseFloatCMG(number){
-	if(number == "" || typeof number == 'underfined'){
+	if(number == "" || typeof number == 'underfined' || isNaN(number)){
 		return 0;
 	}else{
 		return removeCommas(number);
