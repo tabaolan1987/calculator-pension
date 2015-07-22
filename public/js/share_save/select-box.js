5ff96aa1a0ddabe0729ed9688d6438c4b2fdd711
lan.ta <lan.ta@c-mg.com>
2015-07-22 02:51:30 +0100

    update

--- public/js/share_save/select-box.js
+++ public/js/share_save/select-box.js
@@ -43,17 +43,17 @@
 	});
 
 	$(window).bind('resizeEnd', function() {
-	    //do something, window hasn't changed size in 500ms
-	    //alert("aaaaaaa");
-	    //createEditableSelect(dest);
-		var monthlyValue = $('#monthlySavings').val();
-		var duration = $('#durationTime').val();
-	    $("#contain-monthly").empty();
-	    $("#contain-duration").empty();
-	    $("#contain-monthly").append('<input readonly="readonly" class="read-only" type="text" id="monthlySavings" name="monthlySavings" value="'+monthlyValue+'" selectBoxOptions="5.00;10.00;15.00;20.00;25.00;30.00;35.00;40.00;45.00;50.00;55.00;60.00">');
-	    $('#contain-duration').append('<input readonly="readonly" class="read-only" type="text" id="durationTime" name="durationTime" value="'+duration+'" selectBoxOptions="3;5">');
-		drawMonthly();
-		drawSaving();
+		 if ($('#tab1').is(':visible')) {   
+			var monthlyValue = $('#monthlySavings').val();
+			var duration = $('#durationTime').val();
+			$("#contain-monthly").empty();
+			$("#contain-duration").empty();
+			$("#contain-monthly").append('<input readonly="readonly" class="read-only" type="text" id="monthlySavings" name="monthlySavings" value="'+monthlyValue+'" selectBoxOptions="5.00;10.00;15.00;20.00;25.00;30.00;35.00;40.00;45.00;50.00;55.00;60.00">');
+			$('#contain-duration').append('<input readonly="readonly" class="read-only" type="text" id="durationTime" name="durationTime" value="'+duration+'" selectBoxOptions="3;5">');
+			drawMonthly();
+			drawSaving();
+		};  
+		
 	});
 
 	
